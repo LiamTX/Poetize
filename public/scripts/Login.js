e15a6fe144fc42e589397559872508fe442d2fb4
Liam Cabra Teixeira <liam_texeira@estudante.sc.senai.br>
2020-07-29 21:00:32 -0300

    fix login

--- public/scripts/Login.js
+++ public/scripts/Login.js
@@ -15,11 +15,14 @@ document.getElementById('loginBTN').addEventListener('click', async () => {
     await fetch('https://poetize-api.herokuapp.com/api/users/auth', fetchOptions)
         .then(res => res.json())
             .then(result => {
-                await localStorage.setItem(
-                    'session', 
-                    JSON.stringify({id: result.user.id, token: result.token
-                }));
-
-                window.location.href = '/feed';
+                if(result.error){
+                    alert(result.error)
+                }else{
+                    localStorage.setItem(
+                        'session', 
+                        JSON.stringify({id: result.user.id, token: result.token
+                    }));
+                    window.location.href = '/feed';
+                }
             }).catch(error => console.log(error));
 })
